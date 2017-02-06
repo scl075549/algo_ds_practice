785f8ee7e8565230624b2c7c52de7453d6fe694e
Will <william.fanning1@gmail.com>
2017-02-06 10:50:31 -0600

    Add queues text and practice

---
+++ list_based_collections/queues.py
@@ -0,0 +1,14 @@
+"""Queue class implemented using Python list methods"""
+
+class Queue(object):
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        return self.storage.pop(0)
